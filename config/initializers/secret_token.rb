68f54c709235389ac1d2b49372906361e76963bc
ellmo <ellmunzai@gmail.com>
2011-08-03 20:01:59 +0200

    INITIAL COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dtwid::Application.config.secret_token = 'f4ae6f7cc83da197a571ec72eb3d8e489aa3ab8c2e558303cdcf1527173a90ba30fdd50ba3db5f24adcbc5939e513a5b2f3f3913eb86b89f179b317e05f00234'

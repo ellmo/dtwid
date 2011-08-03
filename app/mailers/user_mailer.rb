0ecdcc413a041fdbe97417a399db60db7658bdad
ellmo <ellmunzai@gmail.com>
2011-08-04 01:42:07 +0200

    Adding these might help the cause.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ActionMailer::Base
+  include Devise::Mailers::Helpers
+  default :from => "ellmunzai@gmail.com"
+  
+  def confirmation_instructions(record)
+    devise_mail(record, :confirmation_instructions)
+  end
+
+  def reset_password_instructions(record)
+    devise_mail(record, :reset_password_instructions)
+  end
+
+  def unlock_instructions(record)
+    devise_mail(record, :unlock_instructions)
+  end
+end

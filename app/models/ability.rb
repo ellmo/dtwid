53045a3b6cd233f6980005ab7b0cde64874e2e9a
ellmo <ellmunzai@gmail.com>
2011-08-04 20:13:34 +0200

    User with "user" role HAS to be able to submit new maps.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
+      can :create, Submission
     end
     #
     # The first argument to `can` is the action you are giving the user permission to do.

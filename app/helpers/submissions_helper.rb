0fe689791d6343755065a3d96854b4405951fc3a
ellmo <ellmunzai@gmail.com>
2011-08-06 23:06:52 +0200

    Fixed ASC/DESC sorting toggle in submissions.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,7 +1,7 @@
 module SubmissionsHelper
   def sort_link(title, column, column2=nil)
     sort_dir = params[:d] == 'ASC' ? 'DESC' : 'ASC'
-    request.parameters.reject! {true}
+    request.parameters.reject! {|param| param == "c1" or param == "c2"}
     unless column2
       link_to title, request.parameters.merge( {:c1 => column, :d => sort_dir} )
     else

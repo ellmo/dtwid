b01b6f158b396ef7e7a74cd0d13b611fbb45f95d
ellmo <ellmunzai@gmail.com>
2011-08-07 01:34:59 +0200

    Different colors for each episode.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,4 +4,13 @@ class Submission < ActiveRecord::Base
   belongs_to :map_author
   belongs_to :intended_map_episode, :class_name => "MapEpisode"
   belongs_to :intended_map_slot, :class_name => "MapSlot"
+  
+  def get_css_row_style
+    case self.intended_map_episode_id
+      when 1 then "kditd_row"
+      when 2 then "tsoh_row"
+      when 3 then "inferno_row"
+      else ""
+    end
+  end
 end
